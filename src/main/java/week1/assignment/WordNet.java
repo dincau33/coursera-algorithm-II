83a7a5fc682a038bf91d831222d3df51c28bd77e
dincau33 <dincau33@gmail.com>
2019-11-03 23:22:01 -0800

    week 1 - assignment - implement Wordnet distance and sap methods

--- src/main/java/week1/assignment/WordNet.java
+++ src/main/java/week1/assignment/WordNet.java
@@ -9,46 +9,76 @@ import java.util.Map;
 
 public class WordNet {
 
-    private Map<Integer, String> synsets;
+    private Map<Integer, String> nounById;
+    private Map<String, Integer> idByNoun;
+    private SAP hypernyms;
 
     // constructor takes the name of the two input files
     // Complexity: O((S + H) * log (S + H)) time | O(S + H) space
     public WordNet(String synsets, String hypernyms) {
-        this.synsets = new HashMap<>();
-        loadSynsets(synsets);
+        nounById = new HashMap<>();
+        idByNoun = new HashMap<>();
+        loadNounById(synsets);
+        loadHypernyms(hypernyms, nounById.size());
     }
 
     // returns all WordNet nouns
     public Iterable<String> nouns() {
-        return new ArrayList<String>(synsets.values());
+        return new ArrayList<String>(nounById.values());
     }
 
     // is the word a WordNet noun?
     // Complexity: O(log(N)) time
     public boolean isNoun(String word) {
-        return synsets.containsValue(word);
+        return idByNoun.containsKey(word);
     }
 
     // distance between nounA and nounB (defined below)
     // Complexity: O(S + H) time
     public int distance(String nounA, String nounB) {
-        return -1;
+        if (!isNoun(nounA) || !isNoun(nounB)) throw new IllegalArgumentException();
+        int nounsIdA = idByNoun.get(nounA);
+        int nounsIdB = idByNoun.get(nounB);
+        return hypernyms.length(nounsIdA, nounsIdB);
     }
 
-    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+    // a synset (second field of nounById.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
     // Complexity: O(S + H) time
     public String sap(String nounA, String nounB) {
-        return "";
+        if (!isNoun(nounA) || !isNoun(nounB)) throw new IllegalArgumentException();
+        int nounsIdA = idByNoun.get(nounA);
+        int nounsIdB = idByNoun.get(nounB);
+        int ancestor = hypernyms.ancestor(nounsIdA, nounsIdB);
+        return nounById.get(ancestor);
     }
 
-    private void loadSynsets(String synsets){
+    private void loadNounById(String synsets) {
         In in = new In(synsets);
         while (in.hasNextLine()) {
             String[] line = in.readLine().split(",");
             int id = Integer.valueOf(line[0]);
             String noun = line[1];
-            this.synsets.put(id, noun);
+            nounById.put(id, noun);
+            idByNoun.put(noun, id);
         }
     }
+
+    private void loadHypernyms(String hypernyms, int nounsCount) {
+        Digraph digraph = new Digraph(nounsCount);
+
+        In in = new In(hypernyms);
+        while (in.hasNextLine()) {
+            String[] line = in.readLine().split(",");
+            int v = Integer.valueOf(line[0]);
+            for (int i = 1; i < line.length; i++) {
+                int w = Integer.valueOf(line[i]);
+                digraph.addEdge(v, w);
+            }
+        }
+
+        this.hypernyms = new SAP(digraph);
+    }
+
+
 }
--- src/test/java/week1/assignment/WordNetTest.java
+++ src/test/java/week1/assignment/WordNetTest.java
@@ -16,7 +16,7 @@ class WordNetTest {
     }
 
     @Test
-    void createWordnetTiny() {
+    void createTinyWordnet() {
         WordNet wordNet = createTestDataset("tinySynsets.txt", "tinyHypernyms.txt");
         assertThat(wordNet.isNoun("1750s")).isTrue();
         assertThat(wordNet.isNoun("abc")).isFalse();
@@ -30,6 +30,29 @@ class WordNetTest {
         assertThat(wordNet.isNoun("abc")).isFalse();
         assertThat(wordNet.isNoun("18-karat_gold")).isTrue();
 
+    }
+
+    @Test
+    void failDistanceOrAncestorNotNoun() {
+        WordNet wordNet = createTestDataset("tinySynsets.txt", "tinyHypernyms.txt");
+        assertThrows(IllegalArgumentException.class, () -> wordNet.distance("abc", "1750s"));
+        assertThrows(IllegalArgumentException.class, () -> wordNet.sap("abc", "1750s"));
+    }
+
+    @Test
+    void distanceAndAncestorTinyWordnet() {
+        WordNet wordNet = createTestDataset("tinySynsets.txt", "tinyHypernyms.txt");
+        assertThat(wordNet.distance("1760s", "1820s")).isEqualTo(6);
+        assertThat(wordNet.sap("1760s", "1820s")).isEqualTo("'hood");
+        assertThat(wordNet.distance("1750s", "15_May_Organization")).isEqualTo(2);
+        assertThat(wordNet.sap("1750s", "15_May_Organization")).isEqualTo("1530s");
+    }
+
+    @Test
+    void distanceWordnet() {
+        WordNet wordNet = createTestDataset("synsets.txt", "hypernyms.txt");
+        assertThat(wordNet.distance("AIDS acquired_immune_deficiency_syndrome", "brucellosis undulant_fever Malta_fever Gibraltar_fever Rock_fever Mediterranean_fever")).isEqualTo(2);
+        assertThat(wordNet.sap("AIDS acquired_immune_deficiency_syndrome", "brucellosis undulant_fever Malta_fever Gibraltar_fever Rock_fever Mediterranean_fever")).isEqualTo("infectious_disease");
     }
 
     @Test
